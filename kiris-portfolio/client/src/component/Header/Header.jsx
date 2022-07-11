6abe024f808ff80b4ca43f342e905d29d21c9264
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-07-11 12:39:51 -0700

    outline of header

--- kiris-portfolio/client/src/component/Header/Header.jsx
+++ kiris-portfolio/client/src/component/Header/Header.jsx
@@ -1,6 +1,12 @@
 function Header() {
     return (
-        <p>Header</p>
+        <div id="header">
+            <h1>Kiri's Coding and Traveling Adventures</h1>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#ravels">Travels</a>
+            <a href="#contact">Contact Me</a>
+        </div>
     );
 }
 
