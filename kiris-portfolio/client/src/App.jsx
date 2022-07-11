c23cc8334335302aad1a3ff5915556676066b466
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-07-11 12:51:41 -0700

    outline of Homepage started

--- kiris-portfolio/client/src/App.jsx
+++ kiris-portfolio/client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./component/Header/Header.jsx"
+import Homepage from './pages/Homepage.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Homepage />
     </div>
   );
 }
---
+++ kiris-portfolio/client/src/pages/Homepage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Box } from "@mui/material";
+
+function Homepage() {
+    return (
+        <Box>
+            <Box id="about">
+                <h2>About Me</h2>
+                <p>Hello! My name is Kiriko Anthony and I go by Kiri for short. I have a background in an Anti Money
+                    Laundering (AML) and terrorist financing specialty in financing. I have enjoyed my job as an AML Analyst
+                    and that it is something so impactful to not only our own country but also to the global safety in the
+                    financial industry. I am looking for a job that provides just as meaningful of an outcome but is also
+                    more challenging and allows me to help solve problems daily. I believe that I will be most able to
+                    accheive this within the technology industry, especially through the use of coding.</p>
+            </Box>
+            <Box id="projects">
+                {/* projects section */}
+            </Box>
+            <Box id="travels">
+                {/* travels section */}
+            </Box>
+            <Box id="contact">
+                {/* contact section */}
+            </Box>
+        </Box>
+    );
+}
+
+export default Homepage;
