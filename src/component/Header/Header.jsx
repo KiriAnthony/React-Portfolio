ca603e817b95154734700ccdda84930b1711c7ae
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-08-31 14:23:08 -0700

    updating .gitignore

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <div id="header">
+            <h1 id="title">Kiri's Coding Projects</h1>
+            <div id="nav">
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                {/* <a href="#ravels">Travels</a> */}
+                <a href="#contact">Contact</a>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
