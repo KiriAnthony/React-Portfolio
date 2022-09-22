7ed54362bedbac1db4f2f1cf1dc25f42a0bee483
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-09-22 09:42:49 -0700

    refactor projects setup

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,47 +1,44 @@
-// import runBuddyImg from "../../images/run-buddy_Screenshot.png";
-// import moveiesImg from "../../images/movies screenshot.png";
-// import myPantryImg from "../../images/my-pantry screenshot.png";
-// import wheytsUpImg from "../../images/Wheyts-Up screenshot.png";
-
 const testData = [
     {
-        appLink: "https://wheyts-up.herokuapp.com/",
-        gitHubLink: "",
         title: "Wheyts Up",
-        class: "projects",
+        appLink: "https://wheyts-up.herokuapp.com/",
+        gitHubLink: "https://github.com/BeccaHurtado/wheyts-up",
         image: require('../images/projectScreenshots/WheytsUpScreenshot.png'),
         alt: "Wheyts Up Website Homepage",
         description: "Wheyts Up is designed to allow users to create workout routines and rate them for ease of use at the gym, build by Kiri, Becca, Bryan, and Jace"
     },
     {
-        appLink: "https://whispering-tundra-55246.herokuapp.com/",
         title: "My Pantry",
-        class: "projects",
+        appLink: "https://whispering-tundra-55246.herokuapp.com/",
+        gitHubLink: "https://github.com/MagnificentRoast/My-Pantry",
         image: require('../images/projectScreenshots/myPantryScreenshot.png'),
         alt: "My Pantry Website Homepage",
         description: "My pantry is designed to allow users to access a list of foods they have on the go built by Kiri, Jonathan, Griffin, and Drew."
     },
     {
-        appLink: "https://cwmasters.github.io/moveiesfinal/",
         title: "Moveies",
-        class: "projects",
+        appLink: "https://cwmasters.github.io/moveiesfinal/",
+        gitHubLink: "https://github.com/CWMasters/moveiesfinal",
         image: require('../images/projectScreenshots/moviesScreenshot.png'),
         alt: "Movies Website Homepage",
         description: "Movies is a site to search movies by year built by Kiri, Becca, and Chris. This site utilied third party APIs, teamwork, and communication."
     },
     {
-        appLink: "https://KiriAnthony.github.io/run-buddy",
         title: "Run Buddy",
-        class: "first-project projects",
+        appLink: "https://KiriAnthony.github.io/run-buddy",
+        gitHubLink: "https://github.com/KiriAnthony/run-buddy",
         image: require('../images/projectScreenshots/runBuddyScreenshot.png'),
         alt: "Run-Buddy Website Homepage",
         description: "Run-Buddy is the First Website Built by Kiri. This site specialized in CSS, HTML, and JavaScript."
     }
-    /* <a href="https://wheyts-up.herokuapp.com/"><img
-                title="Budget App"
-                class="projects" src={wheytsUpImg}
-                alt="Budget App Websit Homepage" /></a>
-            <p>Budget App is an app designed to be a simple and easy to use budgeting app that does not need to connect to your bank account. Built using MongoDB, React.js, JSON web Tokens and more by Kiri and Keian.</p> */
-];
+    // {
+    //     title: "Budget App",
+    //     appLink: "",
+    //     gitHubLink: "",
+    //     image: require('../images/projectScreenshots/runBuddyScreenshot.png'),
+    //     alt: "Budget App Website Homepage",
+    //     description: "Budget App is an app designed to be a simple and easy to use budgeting app that does not need to connect to your bank account. Built using MongoDB, React.js, JSON web Tokens and more by Kiri and Keian."
+    // }
+]
 
 export default testData;
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,49 +1,42 @@
 import React from "react";
-// import ProjectArray from "../../Data/Projects";
-// import runBuddyImg from "../../images/run-buddy_Screenshot.png";
-// import moveiesImg from "../../images/movies screenshot.png";
-// import myPantryImg from "../../images/my-pantry screenshot.png";
-// import wheytsUpImg from "../../images/Wheyts-Up screenshot.png";
 
 function Projects(props) {
     return (
         <div id="projects">
-            {console.log(props)}
             {/* projects section */}
             <h2>Projects</h2>
             {/* map through projects array */}
-            {/* {ProjectArray.data.map((item, idx) => {
-                if (item !== null) {
-                    return (
-                        <div className="Projects" key={idx}>
-                        </div>
-                    )
-                } else {
-                    return (
-                        <>
-                            <p>failure</p>
-                        </>
-                    );
+            {
+                props.data.map((item, idx) => {
+                    if (item !== null) {
+                        return (
+                            <div key={idx}>
+                                <div className="project">
+                                    <h3>{item.title}</h3>
+                                    <a href={item.appLink}>
+                                        <img
+                                            title={item.title}
+                                            className="projects"
+                                            src={item.image}
+                                            alt={item.alt}>
+                                        </img>
+                                    </a>
+                                    <p>{item.description}</p>
+                                    <a href={item.gitHubLink}>
+                                        Click Here to View the {item.title} GitHub Repo!
+                                    </a>
+                                </div>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div>
+                                <p>failure</p>
+                            </div>
+                        );
+                    }
                 }
-            })} */}
-            {/* <a href="https://wheyts-up.herokuapp.com/"><img
-                title="Wheyts Up"
-                class="projects" src={wheytsUpImg}
-                alt="Wheyts Up Website Homepage" /></a>
-            <p>Wheyts Up is designed to allow users to create workout routines and rate them for ease of use at the gym, build by Kiri, Becca, Bryan, and Jace</p>
-            <a href="https://whispering-tundra-55246.herokuapp.com/"><img
-                title="My Pantry"
-                class="projects" src={myPantryImg}
-                alt="My Pantry Website Homepage" /></a>
-            <p>My pantry is designed to allow users to access a list of foods they have on the go built by Kiri, Jonathan, Griffin, and Drew.</p>
-            <a href="https://cwmasters.github.io/moveiesfinal/"><img
-                title="Moveies" class="projects"
-                src={moveiesImg} alt="Movies Website Homepage" /></a>
-            <p>Movies is a site to search movies by year built by Kiri, Becca, and Chris. This site utilied third party APIs, teamwork, and communication.</p>
-            <a href="https://KiriAnthony.github.io/run-buddy"><img title="Run Buddy"
-                class="first-project projects" src={runBuddyImg}
-                alt="Run-Buddy Website Homepage" /></a>
-            <p>Run-Buddy is the First Website Built by Kiri. This site specialized in CSS, HTML, and JavaScript.</p> */}
+                )}
         </div>
     );
 }
